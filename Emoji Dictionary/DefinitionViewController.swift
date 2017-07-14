1506983cb698444c25d73a37c3127dd6790d2fbd
Steven Hodgkinson <guitarist_7@yahoo.com>
2017-07-14 01:08:23 +0100

    PrepareForSegueChanges

---
+++ Emoji Dictionary/DefinitionViewController.swift
@@ -0,0 +1,24 @@
+//
+//  DefinitionViewController.swift
+//  Emoji Dictionary
+//
+//  Created by Steven Hodgkinson on 13/07/2017.
+//  Copyright © 2017 Wyrd Entertainment. All rights reserved.
+//
+
+import UIKit
+
+class DefinitionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
