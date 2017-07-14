8de7e93a977355bbee6a14d578a819736b02c4b3
Steven Hodgkinson <guitarist_7@yahoo.com>
2017-07-14 17:50:02 +0100

    End of Section 4

--- Emoji Dictionary/DefinitionViewController.swift
+++ Emoji Dictionary/DefinitionViewController.swift
@@ -10,12 +10,38 @@ import UIKit
 
 class DefinitionViewController: UIViewController {
 
+
+    @IBOutlet weak var definitionLabel: UILabel!
+    
+    @IBOutlet weak var emojiDefLabel: UILabel!
+    
+    
+    var emoji = "No EMoji"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        //print (emoji)
+        emojiDefLabel.text = emoji
+        
+        if emoji == "😀"{
+        definitionLabel.text = "A smiley face"
     }
-
+        if emoji == "😎"{
+        definitionLabel.text = "A cool dude"
+    }
+        if emoji == "😈"{
+        definitionLabel.text = "Happy purple demon"
+    }
+        if emoji == "🐶"{
+            definitionLabel.text = "A puppy"
+    }
+        if emoji == "🦊"{
+        definitionLabel.text = "A Foxy Friend"
+    }
+    
+}
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
